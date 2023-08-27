af6951239aa267d0256f69e295fff913b3e2aeec
Paul Schoenfelder <paulschoenfelder@fastmail.com>
2023-08-27 13:37:45 -0400

    feat: implement liveness analysis

--- ir/src/analysis/mod.rs
+++ ir/src/analysis/mod.rs
@@ -1,7 +1,40 @@
 mod control_flow;
 mod dominance;
+mod liveness;
 mod loops;
 
 pub use self::control_flow::{BlockPredecessor, ControlFlowGraph};
 pub use self::dominance::{DominanceFrontier, DominatorTree, DominatorTreePreorder};
+pub use self::liveness::LivenessAnalysis;
 pub use self::loops::{Loop, LoopAnalysis, LoopLevel};
+
+use crate::hir;
+
+pub struct ControlFlowAnalysis {
+    pub cfg: ControlFlowGraph,
+    pub domtree: DominatorTree,
+    pub loops: LoopAnalysis,
+}
+impl ControlFlowAnalysis {
+    pub fn compute(function: &hir::Function) -> Self {
+        let cfg = ControlFlowGraph::with_function(function);
+        let domtree = DominatorTree::with_function(function, &cfg);
+        let loops = LoopAnalysis::with_function(function, &cfg, &domtree);
+
+        Self {
+            cfg,
+            domtree,
+            loops,
+        }
+    }
+
+    pub fn compute_liveness(&self, function: &hir::Function) -> LivenessAnalysis {
+        LivenessAnalysis::compute(function, self)
+    }
+
+    pub fn recompute(&mut self, function: &hir::Function) {
+        self.cfg.compute(&function.dfg);
+        self.domtree.compute(function, &self.cfg);
+        self.loops.compute(function, &self.cfg, &self.domtree);
+    }
+}
