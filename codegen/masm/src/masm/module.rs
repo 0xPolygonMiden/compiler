c86df15a44ef093a12fb5e906f91b15be4eb6666
Paul Schoenfelder <paulschoenfelder@fastmail.com>
2023-10-09 02:56:18 -0400

    test: implement codegen frontend and tests

--- codegen/masm/src/masm/module.rs
+++ codegen/masm/src/masm/module.rs
@@ -32,3 +32,369 @@ impl Module {
         todo!()
     }
 }
+impl fmt::Display for Module {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        for import in self.imports.iter() {
+            if import.is_aliased() {
+                writeln!(f, "use.{}->{}", import.name, import.alias)?;
+            } else {
+                writeln!(f, "use.{}", import.name)?;
+            }
+        }
+
+        if !self.imports.is_empty() {
+            writeln!(f)?;
+        }
+
+        for function in self.functions.iter() {
+            writeln!(f, "{}\n", function.display(&self.imports))?;
+        }
+
+        if let Some(entry) = self.entry {
+            f.write_str("begin\n")?;
+            writeln!(f, "    exec.{}", entry.function)?;
+            f.write_str("end\n")?;
+        }
+
+        Ok(())
+    }
+}
+
+impl Module {
+    /// This is a helper that constructs and returns the predefined `intrinsics::mem` module
+    pub fn mem_intrinsics() -> Self {
+        use miden_hir::{AbiParam, Felt, FieldElement, Linkage, Signature, Type};
+
+        let mut module = Module::new("intrinsics::mem".parse().unwrap());
+
+        // # Given an element index, and a word, in that order, drop the elements of the
+        // # word other than the at the specified index.
+        // #
+        // # The element index must be in the range 0..=3.
+        // export.extract_element # [element_index, w0, w1, w2, w3]
+
+        let extract_element = "intrinsics::mem::extract_element".parse().unwrap();
+        let mut f = Box::new(Function::new(
+            extract_element,
+            Signature::new(
+                [
+                    AbiParam::new(Type::U8),
+                    AbiParam::new(Type::Felt),
+                    AbiParam::new(Type::Felt),
+                    AbiParam::new(Type::Felt),
+                    AbiParam::new(Type::Felt),
+                ],
+                [AbiParam::new(Type::Felt)],
+            ),
+        ));
+
+        {
+            let body = f.block_mut(f.body);
+            body.extend_from_slice(&[
+                // assert the index given is valid
+                Op::Dup(0),
+                Op::PushU8(3),
+                Op::Lte,
+                Op::Assert,
+                // compute a set of three booleans which used in conjunction with cdrop will
+                // extract the desired element of the given word
+                // # [element_index, w0, ..w3, element_index < 3]
+                Op::Dup(0),
+                Op::PushU8(3),
+                Op::Lt,
+                Op::Movdn(6),
+                // # [element_index, w0, ..w3, element_index < 2, ..]
+                Op::Dup(0),
+                Op::PushU8(2),
+                Op::Lt,
+                Op::Movdn(6),
+                // # [element_index < 1, w0, ..w3, ..]
+                Op::PushU8(1),
+                Op::Lt,
+                // # drop w1 if the element index is zero; or drop w0 if the element index is non-zero
+                Op::Cdrop,
+                // # drop w2 if the element index is one; or drop w0 and w1 if the element index is > 1
+                Op::Movup(4),
+                Op::Cdrop,
+                // # drop w3 if the element index is two; or drop w0, w1, and w2 if the element index is 3
+                // #
+                // # after this point, the only value on the operand stack remaining will be
+                // # the element of the word indicated by the index that was on the top of the
+                // # stack on entry. We've consumed the word itself, as well as the element
+                // # index
+                Op::Movup(3),
+                Op::Cdrop,
+            ]);
+        }
+
+        module.functions.push_back(f);
+
+        // # See `load_felt` for safe usage
+        // proc.load_felt_unchecked # [waddr, index]
+        let load_felt_unchecked = "intrinsics::mem::load_felt_unchecked".parse().unwrap();
+        let mut signature = Signature::new(
+            [AbiParam::new(Type::U32), AbiParam::new(Type::U8)],
+            [AbiParam::new(Type::Felt)],
+        );
+        signature.linkage = Linkage::Internal;
+        let mut f = Box::new(Function::new(load_felt_unchecked, signature));
+
+        {
+            let body = f.block_mut(f.body);
+            body.extend_from_slice(&[
+                // # load the word which contains the desired element
+                // # [w0, w1, w2, w3, index]
+                Op::MemLoadw,
+                // # select the desired element
+                Op::Movup(5),
+                Op::Exec(extract_element),
+            ]);
+        }
+
+        module.functions.push_back(f);
+
+        // # Load a field element from the given native pointer triplet.
+        // #
+        // # A native pointer triplet consists of a word address which contains the
+        // # start of the data; an element index, which indicates which element of
+        // # the word the data starts in; and a byte offset, which indicates which
+        // # byte is the start of the data.
+        // #
+        // # A field element must be naturally aligned, i.e. it's byte offset must be zero.
+        // export.load_felt # [waddr, index, offset]
+        let load_felt = "intrinsics::mem::load_felt".parse().unwrap();
+        let mut f = Box::new(Function::new(
+            load_felt,
+            Signature::new(
+                [
+                    AbiParam::new(Type::U32),
+                    AbiParam::new(Type::U8),
+                    AbiParam::new(Type::U8),
+                ],
+                [AbiParam::new(Type::Felt)],
+            ),
+        ));
+        {
+            let body = f.block_mut(f.body);
+            body.extend_from_slice(&[
+                // # assert the pointer is felt-aligned, then load
+                Op::Movup(2),
+                Op::Assertz,
+                Op::Exec(load_felt_unchecked),
+            ]);
+        }
+
+        module.functions.push_back(f);
+
+        // # Load a single 32-bit machine word from the given native pointer triplet.
+        // #
+        // # A native pointer triplet consists of a word address which contains the
+        // # start of the data; an element index, which indicates which element of
+        // # the word the data starts in; and a byte offset, which indicates which
+        // # byte is the start of the data.
+        // export.load_sw # [waddr, index, offset]
+        let load_sw = "intrinsics::mem::load_sw".parse().unwrap();
+        let mut f = Box::new(Function::new(
+            load_sw,
+            Signature::new(
+                [
+                    AbiParam::new(Type::U32),
+                    AbiParam::new(Type::U8),
+                    AbiParam::new(Type::U8),
+                ],
+                [AbiParam::new(Type::Felt)],
+            ),
+        ));
+        {
+            let is_aligned = f.create_block();
+            let is_unaligned = f.create_block();
+            let is_first_element = f.create_block();
+            let is_not_first_element = f.create_block();
+            let is_second_element = f.create_block();
+            let is_not_second_element = f.create_block();
+            let is_third_element = f.create_block();
+            let is_fourth_element = f.create_block();
+            let body = f.block_mut(f.body);
+            body.extend_from_slice(&[
+                // # check for alignment and offset validity
+                Op::Dup(2),
+                Op::EqImm(Felt::ZERO),
+                Op::Dup(3),
+                Op::PushU8(8),
+                Op::U32CheckedLt,
+                // # offset must be < 8
+                Op::Assert,
+                // # if the pointer is naturally aligned..
+                Op::If(is_aligned, is_unaligned),
+            ]);
+
+            let is_aligned = f.block_mut(is_aligned);
+            is_aligned.extend_from_slice(&[
+                // # drop the byte offset
+                Op::Movup(2),
+                Op::Drop,
+                // # load the element containing the data we want
+                Op::Exec(load_felt_unchecked),
+            ]);
+
+            let is_unaligned = f.block_mut(is_unaligned);
+            is_unaligned.extend_from_slice(&[
+                // # check if the load starts in the first element
+                Op::Dup(1),
+                Op::EqImm(Felt::ZERO),
+                Op::If(is_first_element, is_not_first_element),
+            ]);
+
+            let is_first_element = f.block_mut(is_first_element);
+            is_first_element.extend_from_slice(&[
+                // # the load is across both the first and second elements
+                // # drop the element index
+                Op::Swap(1),
+                Op::Drop,
+                // # load a word
+                // # [w0, w1, w2, w3, offset]
+                Op::MemLoadw,
+                // # drop the unused elements
+                Op::Movup(4),
+                Op::Movup(4),
+                Op::Drop,
+                Op::Drop,
+                // # shift high bits left by the offset
+                // # [hi, w1, offset]
+                Op::Dup(2),
+                Op::U32CheckedShl,
+                // # move the low bits to the top and shift them as well
+                // # [offset, 32, w1, hi]
+                Op::Swap(1),
+                Op::PushU8(32),
+                Op::Movup(2),
+                // # [32 - offset, w1, hi]
+                Op::U32CheckedSub,
+                // # [lo, hi]
+                Op::U32CheckedShr,
+                // # combine the two halves
+                // # [result]
+                Op::U32Or,
+            ]);
+
+            let is_not_first_element = f.block_mut(is_not_first_element);
+            is_not_first_element.extend_from_slice(&[
+                // # check if the load starts in the second element
+                Op::Dup(1),
+                Op::EqImm(Felt::ONE),
+                Op::If(is_second_element, is_not_second_element),
+            ]);
+
+            let is_second_element = f.block_mut(is_second_element);
+            is_second_element.extend_from_slice(&[
+                // # the load is across both the second and third elements
+                // # drop the element idnex
+                Op::Swap(1),
+                Op::Drop,
+                // # load
+                // # [w0, w1, w2, w3, offset]
+                Op::MemLoadw,
+                // # drop the unused elements
+                // # [w1, w2, offset]
+                Op::Drop,
+                Op::Movdn(3),
+                Op::Movdn(3),
+                Op::Drop,
+                // # shift the high bits
+                // # [hi, w2, offset]
+                Op::Dup(2),
+                Op::U32CheckedShl,
+                // # shift the low bits
+                // # [offset, 32, w2, hi]
+                Op::Swap(1),
+                Op::PushU8(32),
+                Op::Movup(2),
+                // # [32 - offset, w2, hi]
+                Op::U32CheckedSub,
+                // # [lo, hi]
+                Op::U32CheckedShr,
+                // # combine the two halves
+                // # [result]
+                Op::U32Or,
+            ]);
+
+            let is_not_second_element = f.block_mut(is_not_second_element);
+            is_not_second_element.extend_from_slice(&[
+                // # check if the load starts in the third element
+                Op::Swap(1),
+                Op::EqImm(Felt::new(2)),
+                Op::If(is_third_element, is_fourth_element),
+            ]);
+
+            let is_third_element = f.block_mut(is_third_element);
+            is_third_element.extend_from_slice(&[
+                // # the load is across both the third and fourth elements
+                // # [w0, w1, w2, w3, offset]
+                Op::MemLoadw,
+                // # drop first two unused
+                // # [w2, w3, offset]
+                Op::Drop,
+                Op::Drop,
+                // # shift the high bits
+                // # [hi, w3, offset]
+                Op::Dup(2),
+                Op::U32CheckedShl,
+                // # shift the low bits
+                // # [offset, 32, w3, hi]
+                Op::Swap(1),
+                Op::PushU8(32),
+                Op::Movup(2),
+                // # [32 - offset, w3, hi]
+                Op::U32CheckedSub,
+                // # [lo, hi]
+                Op::U32CheckedShr,
+                // # combine the two halves
+                // # [result]
+                Op::U32Or,
+            ]);
+
+            let is_fourth_element = f.block_mut(is_fourth_element);
+            is_fourth_element.extend_from_slice(&[
+                // # the load crosses a word boundary
+                // # start with the word containing the low bits
+                // # [waddr, waddr, offset]
+                Op::Dup(0),
+                // # [waddr + 1, waddr, offset]
+                Op::U32CheckedAddImm(1),
+                // # load the word and drop the unused elements
+                // # [w0, waddr, offset]
+                Op::MemLoadw,
+                Op::Movdn(4),
+                Op::Drop,
+                Op::Drop,
+                Op::Drop,
+                // # shift the low bits
+                // # [offset, 32, w0, waddr, offset]
+                Op::PushU8(32),
+                Op::Dup(3),
+                // # [32 - offset, w0, waddr, offset]
+                Op::U32CheckedSub,
+                // # [lo, waddr, offset]
+                Op::U32CheckedShr,
+                // # load the word with the high bits, drop unused elements
+                // # [w3, lo, offset]
+                Op::Swap(1),
+                Op::MemLoadw,
+                Op::Drop,
+                Op::Drop,
+                Op::Drop,
+                // # shift high bits
+                // # [hi, lo]
+                Op::Movup(2),
+                Op::U32CheckedShl,
+                // # combine the two halves
+                // # [result]
+                Op::U32Or,
+            ]);
+        }
+
+        module.functions.push_back(f);
+
+        module
+    }
+}
---
+++ codegen/masm/src/tests.rs
@@ -0,0 +1,413 @@
+use miden_hir::{
+    self,
+    testing::{self, TestContext},
+    AbiParam, Felt, FieldElement, Immediate, InstBuilder, OperandStack, ProgramBuilder, Signature,
+    SourceSpan, Stack, StarkField, Type,
+};
+use miden_hir_analysis::FunctionAnalysis;
+use std::fmt::Write;
+
+use super::*;
+
+#[cfg(test)]
+#[allow(unused_macros)]
+macro_rules! assert_masm_output {
+    ($lhs:expr, $rhs:expr) => {{
+        let lhs = $lhs;
+        let rhs = $rhs;
+        if lhs != rhs {
+            panic!(
+                r#"
+assertion failed: `(left matches right)`
+left: `{}`,
+right: `{}`"#,
+                lhs, rhs
+            );
+        }
+    }};
+}
+
+#[derive(Default)]
+struct TestByEmulationHarness {
+    context: TestContext,
+    emulator: Emulator,
+}
+impl TestByEmulationHarness {
+    #[allow(unused)]
+    pub fn with_emulator_config(memory_size: usize, hp: usize, lp: usize) -> Self {
+        Self {
+            context: TestContext::default(),
+            emulator: Emulator::new(
+                memory_size.try_into().expect("invalid memory size"),
+                hp.try_into().expect("invalid address"),
+                lp.try_into().expect("invalid address"),
+            ),
+        }
+    }
+
+    pub fn apply_rewrite_passes(
+        &self,
+        function: &mut hir::Function,
+        analysis: &mut FunctionAnalysis,
+    ) -> anyhow::Result<()> {
+        use miden_hir_transform::{self as transform, RewritePass};
+
+        let mut rewrites = transform::SplitCriticalEdges
+            .chain(transform::Treeify)
+            .chain(transform::InlineBlocks);
+        rewrites.run(function, analysis)
+    }
+
+    pub fn stackify(
+        &self,
+        program: &hir::Program,
+        function: &mut hir::Function,
+    ) -> anyhow::Result<Box<Function>> {
+        use miden_hir_pass::Pass;
+
+        // Analyze function
+        let mut analysis = FunctionAnalysis::new(function);
+
+        // Apply pre-codegen transformations
+        self.apply_rewrite_passes(function, &mut analysis)?;
+
+        let mut original = String::with_capacity(1024);
+        miden_hir::write_function(&mut original, &function).expect("formatting failed");
+        println!("{}", &original);
+
+        // Make sure all analyses are available
+        analysis.ensure_all(&function);
+
+        // Run stackification
+        let mut pass = Stackify::new(program, &analysis);
+        pass.run(function)
+    }
+
+    pub fn malloc(&mut self, size: usize) -> u32 {
+        self.emulator.malloc(size)
+    }
+
+    #[inline(always)]
+    pub fn store(&mut self, addr: usize, index: usize, value: Felt) {
+        self.emulator.store(addr, index, value);
+    }
+
+    pub fn execute(
+        mut self,
+        program: Program,
+        args: &[Felt],
+    ) -> Result<OperandStack<Felt>, EmulationError> {
+        let entrypoint = program.entrypoint.expect("cannot execute a library");
+        self.emulator
+            .load_program(program)
+            .expect("failed to load program");
+        self.emulator.invoke(entrypoint, args)
+    }
+
+    pub fn execute_module(
+        mut self,
+        module: Module,
+        args: &[Felt],
+    ) -> Result<OperandStack<Felt>, EmulationError> {
+        let entrypoint = module.entry.expect("cannot execute a library");
+        self.emulator
+            .load_module(module)
+            .expect("failed to load module");
+        self.emulator.invoke(entrypoint, args)
+    }
+}
+
+/// Test the emulator on the fibonacci function
+#[test]
+fn fib_emulator() {
+    let harness = TestByEmulationHarness::default();
+
+    // Build a simple program
+    let mut builder = ProgramBuilder::new(&harness.context.diagnostics);
+
+    // Build test module with fib function
+    let mut mb = builder.module("test");
+    testing::fib1(mb.as_mut(), &harness.context.diagnostics);
+    mb.build()
+        .expect("unexpected error constructing test module");
+
+    // Link the program
+    let mut program = builder
+        .with_entrypoint("test::fib".parse().unwrap())
+        .link()
+        .expect("failed to link program");
+
+    // Compile to MASM IR
+    let mut compiler = MasmCompiler::new(&harness.context.diagnostics);
+    let masm = compiler.compile(&mut program).expect("compilation failed");
+
+    // Test it via the emulator
+    let n = Felt::new(10);
+    harness.execute(masm, &[n]).expect("execution failed");
+}
+
+/// Test the [Stackify] pass on a very simple program with a conditional as a sanity check
+#[test]
+fn stackify_fundamental_if() {
+    let harness = TestByEmulationHarness::default();
+
+    // Build a simple program
+    let mut builder = ProgramBuilder::new(&harness.context.diagnostics);
+
+    // Build test module with function that adds two numbers if the
+    // first number is odd, and multiplies them if the first number is even
+    let mut mb = builder.module("test");
+    let id = {
+        let mut fb = mb
+            .function(
+                "add_odd_mul_even",
+                Signature::new(
+                    [AbiParam::new(Type::U32), AbiParam::new(Type::U32)],
+                    [AbiParam::new(Type::U32)],
+                ),
+            )
+            .expect("unexpected symbol conflict");
+        let entry = fb.current_block();
+        let (a, b) = {
+            let args = fb.block_params(entry);
+            (args[0], args[1])
+        };
+        let is_odd_blk = fb.create_block();
+        let is_even_blk = fb.create_block();
+        let is_odd = fb.ins().is_odd(a, SourceSpan::UNKNOWN);
+        fb.ins().cond_br(
+            is_odd,
+            is_odd_blk,
+            &[],
+            is_even_blk,
+            &[],
+            SourceSpan::UNKNOWN,
+        );
+        fb.switch_to_block(is_odd_blk);
+        fb.ins().add(a, b, SourceSpan::UNKNOWN);
+        fb.switch_to_block(is_even_blk);
+        fb.ins().mul(a, b, SourceSpan::UNKNOWN);
+        fb.build().expect("unexpected error building function")
+    };
+
+    mb.build()
+        .expect("unexpected error constructing test module");
+
+    // Link the program
+    let mut program = builder
+        .with_entrypoint(id)
+        .link()
+        .expect("failed to link program");
+
+    // Get the sum_matrix function
+    let mut function = {
+        let modules = program.modules_mut();
+        let mut test = modules.find_mut("test").remove().expect("undefined module");
+        let function = test
+            .cursor_mut_at(id.function)
+            .remove()
+            .expect("undefined function");
+        modules.insert(test);
+        function
+    };
+
+    let masm = harness
+        .stackify(&program, &mut function)
+        .expect("stackification failed");
+
+    let mut module = Module::new(id.module);
+    module.functions.push_back(masm);
+    module.entry = Some(id);
+
+    let a = Felt::new(3);
+    let b = Felt::new(4);
+
+    let mut stack = harness
+        .execute_module(module, &[a, b])
+        .expect("execution failed");
+    assert_eq!(stack.len(), 1);
+    assert_eq!(stack.pop().map(|e| e.as_int()), Some(12));
+}
+
+/// Test the [Stackify] pass on a very simple program with a loop as a sanity check
+#[test]
+fn stackify_fundamental_loops() {
+    let harness = TestByEmulationHarness::default();
+
+    // Build a simple program
+    let mut builder = ProgramBuilder::new(&harness.context.diagnostics);
+
+    // Build test module with function that increments a number until a
+    // given iteration count is reached
+    let mut mb = builder.module("test");
+    let id = {
+        let mut fb = mb
+            .function(
+                "incr_until",
+                Signature::new(
+                    [AbiParam::new(Type::U32), AbiParam::new(Type::U32)],
+                    [AbiParam::new(Type::U32)],
+                ),
+            )
+            .expect("unexpected symbol conflict");
+        let entry = fb.current_block();
+        let (a, n) = {
+            let args = fb.block_params(entry);
+            (args[0], args[1])
+        };
+        let loop_header_blk = fb.create_block();
+        let a1 = fb.append_block_param(loop_header_blk, Type::U32, SourceSpan::UNKNOWN);
+        let n1 = fb.append_block_param(loop_header_blk, Type::U32, SourceSpan::UNKNOWN);
+        let loop_body_blk = fb.create_block();
+        let loop_exit_blk = fb.create_block();
+        let result0 = fb.append_block_param(loop_exit_blk, Type::U32, SourceSpan::UNKNOWN);
+        fb.ins().br(loop_header_blk, &[a, n], SourceSpan::UNKNOWN);
+
+        fb.switch_to_block(loop_header_blk);
+        let is_zero = fb.ins().eq_imm(n1, Immediate::U32(0), SourceSpan::UNKNOWN);
+        fb.ins().cond_br(
+            is_zero,
+            loop_exit_blk,
+            &[a1],
+            loop_body_blk,
+            &[],
+            SourceSpan::UNKNOWN,
+        );
+
+        fb.switch_to_block(loop_body_blk);
+        let a2 = fb.ins().incr(a1, SourceSpan::UNKNOWN);
+        let n2 = fb.ins().sub_imm(n1, Immediate::U32(1), SourceSpan::UNKNOWN);
+        fb.ins().br(loop_header_blk, &[a2, n2], SourceSpan::UNKNOWN);
+
+        fb.switch_to_block(loop_exit_blk);
+        fb.ins().ret(Some(result0), SourceSpan::UNKNOWN);
+
+        fb.build().expect("unexpected error building function")
+    };
+
+    mb.build()
+        .expect("unexpected error constructing test module");
+
+    // Link the program
+    let mut program = builder
+        .with_entrypoint(id)
+        .link()
+        .expect("failed to link program");
+
+    // Get the sum_matrix function
+    let (mut function, imports) = {
+        let modules = program.modules_mut();
+        let mut test = modules.find_mut("test").remove().expect("undefined module");
+        let function = test
+            .cursor_mut_at(id.function)
+            .remove()
+            .expect("undefined function");
+        let imports = test.imports();
+        modules.insert(test);
+        (function, imports)
+    };
+
+    let masm = harness
+        .stackify(&program, &mut function)
+        .expect("stackification failed");
+
+    let mut output = String::with_capacity(1024);
+    write!(&mut output, "{}", masm.display(&imports)).expect("formatting failed");
+
+    println!("{}", output.as_str());
+
+    let mut module = Module::new(id.module);
+    module.functions.push_back(masm);
+    module.entry = Some(id);
+
+    let a = Felt::new(3);
+    let n = Felt::new(4);
+
+    let mut stack = harness
+        .execute_module(module, &[a, n])
+        .expect("execution failed");
+    assert_eq!(stack.len(), 1);
+    assert_eq!(stack.pop().map(|e| e.as_int()), Some(7));
+}
+
+/// Test the [Stackify] pass on a simple program containing [testing::sum_matrix].
+#[test]
+fn stackify_sum_matrix() {
+    let mut harness = TestByEmulationHarness::default();
+
+    harness
+        .emulator
+        .load_module(Module::mem_intrinsics())
+        .expect("failed to load intrinsics::mem");
+
+    // Build a simple program
+    let mut builder = ProgramBuilder::new(&harness.context.diagnostics);
+
+    // Build test module with fib function
+    let mut mb = builder.module("test");
+    let id = testing::sum_matrix(mb.as_mut(), &harness.context.diagnostics);
+    mb.build()
+        .expect("unexpected error constructing test module");
+
+    // Link the program
+    let mut program = builder
+        .with_entrypoint("test::sum_matrix".parse().unwrap())
+        .link()
+        .expect("failed to link program");
+
+    // Get the sum_matrix function
+    let (mut function, imports) = {
+        let modules = program.modules_mut();
+        let mut test = modules.find_mut("test").remove().expect("undefined module");
+        let function = test
+            .cursor_mut_at(id.function)
+            .remove()
+            .expect("undefined function");
+        let imports = test.imports();
+        modules.insert(test);
+        (function, imports)
+    };
+
+    let masm = harness
+        .stackify(&program, &mut function)
+        .expect("stackification failed");
+
+    let mut output = String::with_capacity(1024);
+    write!(&mut output, "{}", masm.display(&imports)).expect("formatting failed");
+
+    println!("{}", output.as_str());
+
+    let mut module = Module::new(id.module);
+    module.functions.push_back(masm);
+    module.entry = Some(id);
+
+    let addr = harness.malloc(core::mem::size_of::<u32>() * 3 * 3);
+    let ptr = Felt::new(addr as u64);
+    let rows = Felt::new(3);
+    let cols = Felt::new(3);
+
+    // [1, 0, 1,
+    //  0, 1, 0,
+    //  1, 1, 1] == 6
+    let addr = addr as usize;
+    harness.store(addr, 0, Felt::ONE);
+    harness.store(addr, 1, Felt::ZERO);
+    harness.store(addr, 2, Felt::ONE);
+    harness.store(addr, 3, Felt::ZERO);
+    harness.store(addr + 1, 0, Felt::ONE);
+    harness.store(addr + 1, 1, Felt::ZERO);
+    harness.store(addr + 1, 2, Felt::ONE);
+    harness.store(addr + 1, 3, Felt::ONE);
+    harness.store(addr + 2, 0, Felt::ONE);
+
+    assert_eq!(3 % 2, 0);
+    let mut stack = harness
+        .execute_module(module, &[ptr, rows, cols])
+        .expect("execution failed");
+    assert_eq!(stack.len(), 1);
+    assert_eq!(stack.pop().map(|e| e.as_int()), Some(6));
+
+    //let expected = "";
+
+    //assert_masm_output!(expected, output.as_str());
+}
