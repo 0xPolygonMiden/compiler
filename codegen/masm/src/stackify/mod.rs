8a71b6288d35b6f2ba66b8e9d66d5fd46dfb0be2
Paul Schoenfelder <paulschoenfelder@fastmail.com>
2023-10-09 02:46:41 -0400

    refactor: rewrite operand stack impl for codegen
    This rewrite makes the operand stack provide a type-agnostic,
    operand-oriented API, but internally is type-aware, and ensures
    that the layout of operands on the stack is taken into account
    when moving things around. It also enhances the representation
    of values on the stack to provide richer metadata. In addition,
    in order to represent transient values on the stack which do
    not have an SSA value, or constant value, associated with them,
    we also allow tracking the abstract types of such values on the
    stack, while limiting the contexts in which we are allowed to
    observe them.

--- codegen/masm/src/stackify/mod.rs
+++ codegen/masm/src/stackify/mod.rs
@@ -4,6 +4,6 @@ mod pass;
 mod treegraph;
 
 pub(crate) use self::dependency_graph::{Dependency, DependencyGraph, DependencyId, Node};
-pub(crate) use self::operand_stack::{Operand, OperandStack};
+pub(crate) use self::operand_stack::{Operand, OperandStack, OperandType, TypedValue};
 pub use self::pass::Stackify;
 pub(crate) use self::treegraph::TreeGraph;
