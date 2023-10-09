4c12592757d364e5c1c0bc50697ee99c1357e560
Paul Schoenfelder <paulschoenfelder@fastmail.com>
2023-10-09 02:44:45 -0400

    chore: update masm ir program/module/function items

--- codegen/masm/src/masm/module.rs
+++ codegen/masm/src/masm/module.rs
@@ -1,14 +1,18 @@
-use intrusive_collections::LinkedList;
-use miden_hir::Ident;
-use std::collections::BTreeSet;
+use core::fmt;
 
-use super::{FunctionListAdapter, Import};
+use intrusive_collections::LinkedList;
+use miden_hir::{FunctionIdent, Ident};
+
+use super::{Function, FunctionListAdapter, ModuleImportInfo, Op};
 
 pub struct Module {
     /// The name of this module, e.g. `std::math::u64`
     pub name: Ident,
+    /// If this module contains a program entrypoint, this is the
+    /// function identifier which should be used for that purpose.
+    pub entry: Option<FunctionIdent>,
     /// The modules to import, along with their local aliases
-    pub imports: BTreeSet<Import>,
+    pub imports: ModuleImportInfo,
     /// The functions defined in this module
     pub functions: LinkedList<FunctionListAdapter>,
 }
@@ -17,8 +21,14 @@ impl Module {
     pub fn new(name: Ident) -> Self {
         Self {
             name,
+            entry: None,
             imports: Default::default(),
             functions: Default::default(),
         }
+    }
+
+    /// Write this module as Miden Assembly text to `out`
+    pub fn emit(&self, _out: &mut dyn std::io::Write) -> std::io::Result<()> {
+        todo!()
     }
 }
