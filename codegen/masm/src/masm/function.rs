cece54b1df7f38bcda59dedd75aeb39e34365ee7
Paul Schoenfelder <paulschoenfelder@fastmail.com>
2023-10-16 22:38:12 -0400

    feat: implement conversion of masm ir to 'real' masm

--- codegen/masm/src/masm/function.rs
+++ codegen/masm/src/masm/function.rs
@@ -2,7 +2,9 @@ use std::fmt;
 
 use cranelift_entity::{EntityRef, PrimaryMap};
 use intrusive_collections::{intrusive_adapter, LinkedListLink};
+use miden_diagnostics::Spanned;
 use miden_hir::{FunctionIdent, Signature, Type};
+use rustc_hash::FxHashMap;
 use smallvec::{smallvec, SmallVec};
 
 use super::*;
@@ -10,9 +12,11 @@ use super::*;
 intrusive_adapter!(pub FunctionListAdapter = Box<Function>: Function { link: LinkedListLink });
 
 /// This represents a function in Miden Assembly
+#[derive(Spanned)]
 pub struct Function {
     link: LinkedListLink,
     /// The name of this function
+    #[span]
     pub name: FunctionIdent,
     /// The type signature of this function
     pub signature: Signature,
@@ -115,7 +119,92 @@ impl Function {
             imports,
         }
     }
+
+    pub fn to_function_ast(
+        &self,
+        codemap: &miden_diagnostics::CodeMap,
+        imports: &miden_hir::ModuleImportInfo,
+        local_ids: &FxHashMap<FunctionIdent, u16>,
+        proc_ids: &FxHashMap<FunctionIdent, miden_assembly::ProcedureId>,
+    ) -> miden_assembly::ast::ProcedureAst {
+        use miden_assembly::{
+            self as masm,
+            ast::{ProcedureAst, SourceLocation},
+        };
+
+        let name = masm::ProcedureName::try_from(self.name.function.as_str())
+            .expect("invalid function name");
+        let num_locals = u16::try_from(self.locals.len()).expect("too many locals");
+        let start = codemap
+            .location(self)
+            .ok()
+            .map(|loc| {
+                SourceLocation::new(loc.line.to_usize() as u32, loc.column.to_usize() as u32)
+            })
+            .unwrap_or_default();
+        let body = emit_block(
+            self.body,
+            &self.blocks,
+            codemap,
+            imports,
+            local_ids,
+            proc_ids,
+        );
+
+        ProcedureAst {
+            name,
+            docs: None,
+            num_locals,
+            body,
+            start,
+            is_export: self.signature.is_public(),
+        }
+    }
 }
+
+fn emit_block(
+    block_id: BlockId,
+    blocks: &PrimaryMap<BlockId, Block>,
+    codemap: &miden_diagnostics::CodeMap,
+    imports: &miden_hir::ModuleImportInfo,
+    local_ids: &FxHashMap<FunctionIdent, u16>,
+    proc_ids: &FxHashMap<FunctionIdent, miden_assembly::ProcedureId>,
+) -> miden_assembly::ast::CodeBody {
+    use miden_assembly::ast::{CodeBody, Node};
+
+    let current_block = &blocks[block_id];
+    let mut ops = Vec::with_capacity(current_block.ops.len());
+    for op in current_block.ops.iter() {
+        match op.clone() {
+            Op::If(then_blk, else_blk) => {
+                let true_case = emit_block(then_blk, blocks, codemap, imports, local_ids, proc_ids);
+                let false_case =
+                    emit_block(else_blk, blocks, codemap, imports, local_ids, proc_ids);
+                ops.push(Node::IfElse {
+                    true_case,
+                    false_case,
+                });
+            }
+            Op::While(blk) => {
+                let body = emit_block(blk, blocks, codemap, imports, local_ids, proc_ids);
+                ops.push(Node::While { body });
+            }
+            Op::Repeat(n, blk) => {
+                let body = emit_block(blk, blocks, codemap, imports, local_ids, proc_ids);
+                ops.push(Node::Repeat {
+                    times: n as u32,
+                    body,
+                });
+            }
+            op => {
+                ops.extend(op.into_node(codemap, imports, local_ids, proc_ids));
+            }
+        }
+    }
+
+    CodeBody::new(ops)
+}
+
 impl fmt::Debug for Function {
     fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
         f.debug_struct("Function")
--- codegen/masm/src/masm/program.rs
+++ codegen/masm/src/masm/program.rs
@@ -1,16 +1,23 @@
-use std::path::Path;
+use std::{collections::BTreeMap, path::Path};
 
-use miden_hir::{self as hir, DataSegmentTable, FunctionIdent};
+use miden_hir::{self as hir, DataSegmentTable, FunctionIdent, Ident};
 
 use super::*;
 
+/// A [Program] represents a complete set of modules which are intended to
+/// be shipped together as an artifact, either as an executable, or as a library
+/// to be integrated into a larger executable.
 #[derive(Default)]
 pub struct Program {
+    /// The set of modules which belong to this program
     pub modules: Vec<Module>,
+    /// The function identifier for the program entrypoint, if this is an executable module
     pub entrypoint: Option<FunctionIdent>,
+    /// The data segment table for this program
     pub segments: DataSegmentTable,
 }
 impl Program {
+    /// Create a new, empty [Program]
     pub fn new() -> Self {
         Self::default()
     }
@@ -21,6 +28,66 @@ impl Program {
 
     pub fn is_library(&self) -> bool {
         self.entrypoint.is_none()
+    }
+
+    /// Write this [Program] to the given output directory.
+    ///
+    /// The provided [miden_diagnostics::CodeMap] is used for computing source locations.
+    pub fn write_to_directory<P: AsRef<Path>>(
+        &self,
+        codemap: &miden_diagnostics::CodeMap,
+        path: P,
+    ) -> std::io::Result<()> {
+        use miden_assembly as masm;
+
+        let path = path.as_ref();
+        assert!(path.is_dir());
+
+        let program = self.to_program_ast();
+        program.write_to_file(path.join(masm::LibraryPath::EXEC_PATH))?;
+
+        for module in self.modules.iter() {
+            module.write_to_directory(codemap, path)?;
+        }
+
+        Ok(())
+    }
+
+    /// Convert this program to its [miden_assembly::ast::ProgramAst] representation
+    pub fn to_program_ast(&self) -> miden_assembly::ast::ProgramAst {
+        use miden_assembly::{
+            self as masm,
+            ast::{Instruction, ModuleImports, Node, ProgramAst},
+        };
+
+        if let Some(entry) = self.entrypoint {
+            let entry_import = Import::try_from(entry.module).expect("invalid module name");
+            let entry_module_path =
+                masm::LibraryPath::new(entry_import.name.as_str()).expect("invalid module path");
+            let entry_id =
+                masm::ProcedureId::from_name(entry.function.as_str(), &entry_module_path);
+            let entry_name = masm::ProcedureName::try_from(
+                FunctionIdent {
+                    module: Ident::with_empty_span(entry_import.alias),
+                    ..entry
+                }
+                .to_string(),
+            )
+            .expect("invalid entrypoint function name");
+            let imported =
+                BTreeMap::from([(entry_import.alias.to_string(), entry_module_path.clone())]);
+            let invoked = BTreeMap::from([(entry_id, (entry_name, entry_module_path))]);
+            let imports = ModuleImports::new(imported, invoked);
+
+            // TODO: Write data segments, initialize function table
+            let body = vec![Node::Instruction(Instruction::ExecImported(entry_id))];
+
+            ProgramAst::new(body, vec![])
+                .expect("invalid program")
+                .with_import_info(imports)
+        } else {
+            todo!("0xPolygonMiden/miden-vm#1108")
+        }
     }
 }
 impl From<&hir::Program> for Program {
@@ -35,8 +102,4 @@ impl From<&hir::Program> for Program {
     }
 }
 
-impl Program {
-    pub fn write_to_directory<P: AsRef<Path>>(&self, _path: P) -> std::io::Result<()> {
-        todo!()
-    }
-}
+impl Program {}
