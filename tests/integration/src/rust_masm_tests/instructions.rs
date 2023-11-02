5dcda35f752c78ad87f4eb842540058c56760ec2
Denys Zadorozhnyi <denys@zadorozhnyi.com>
2023-11-02 17:26:11 +0200

    test: run MASM on VM vs native Rust under `proptest` in integration tests

--- tests/integration/src/rust_masm_tests/instructions.rs
+++ tests/integration/src/rust_masm_tests/instructions.rs
@@ -1,8 +1,9 @@
 use expect_test::expect_file;
 use miden_hir::Felt;
-use miden_hir::StarkField;
+use proptest::prop_assert_eq;
+use proptest::test_runner::TestRunner;
 
-use crate::execute_emulator;
+use crate::execute_vm;
 use crate::CompilerTest;
 
 #[test]
@@ -12,13 +13,18 @@ fn i32_gt_u() {
     test.expect_wasm(expect_file!["../../expected/gt_u.wat"]);
     test.expect_ir(expect_file!["../../expected/gt_u.hir"]);
     test.expect_masm(expect_file!["../../expected/gt_u.masm"]);
-    let ir_masm = test.codegen_masm_program();
+    // let ir_masm = test.codegen_masm_program();
+    let vm_program = test.vm_masm_program();
 
-    // TODO: run via proptest when emulator doesn't consume the program
-    let a: u32 = 8;
-    let b: u32 = 3;
-    let rust_out = (a > b) as u64;
-    let args = [Felt::from(a), Felt::from(b)];
-    let emulator_out = execute_emulator(ir_masm, &args).first().unwrap().clone();
-    assert_eq!(rust_out, emulator_out.as_int());
+    // Run the Rust and compiled MASM code against a bunch of random inputs and compare the results
+    TestRunner::default()
+        .run(&(0..(u32::MAX / 2), 0..(u32::MAX / 2)), move |(a, b)| {
+            let rust_out = (a > b) as u64;
+            let mut args = [Felt::from(a), Felt::from(b)];
+            args.reverse();
+            let vm_out = execute_vm(&vm_program, &args).first().unwrap().clone();
+            prop_assert_eq!(rust_out, vm_out);
+            Ok(())
+        })
+        .unwrap();
 }
