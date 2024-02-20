dc9d1d640bff44ce82b31d525bd14475783d4d6d
Denys Zadorozhnyi <dzadorozhnyi@polygon.technology>
2024-02-20 16:53:08 +0200

    refactor: merge `conversions` interface into `types` interface

--- tests/rust-apps-wasm/sdk/sdk/src/lib.rs
+++ tests/rust-apps-wasm/sdk/sdk/src/lib.rs
@@ -10,14 +10,26 @@ fn my_panic(_info: &core::panic::PanicInfo) -> ! {
 
 mod bindings;
 
-use bindings::exports::miden::base::core_types::Guest;
-use bindings::exports::miden::base::core_types::{AccountId, Felt};
+use bindings::exports::miden::base::core_types;
+use bindings::exports::miden::base::core_types::{AccountId, CoreAsset, Felt};
+use bindings::exports::miden::base::types;
+use bindings::exports::miden::base::types::Asset;
 
 pub struct Component;
 
-impl Guest for Component {
+impl core_types::Guest for Component {
     fn account_id_from_felt(felt: Felt) -> AccountId {
         // TODO: assert that felt is a valid account id
         AccountId { inner: felt }
+    }
+}
+
+impl types::Guest for Component {
+    fn from_core_asset(asset: CoreAsset) -> Asset {
+        todo!()
+    }
+
+    fn to_core_asset(asset: Asset) -> CoreAsset {
+        todo!()
     }
 }
