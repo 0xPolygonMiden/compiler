cb0fd92ca932e87a1d08195ea35ae9c1c1ed6bf5
Paul Schoenfelder <paulschoenfelder@fastmail.com>
2024-08-14 06:12:46 -0400

    feat(debugger): improve argument parsing for read command

--- midenc-runner/src/debug/memory.rs
+++ midenc-runner/src/debug/memory.rs
@@ -1,5 +1,10 @@
-use std::str::FromStr;
+use std::{
+    ffi::{OsStr, OsString},
+    fmt,
+    str::FromStr,
+};
 
+use clap::{Parser, ValueEnum};
 use midenc_codegen_masm::NativePtr;
 use midenc_hir::Type;
 
@@ -15,116 +20,95 @@ impl FromStr for ReadMemoryExpr {
     type Err = String;
 
     fn from_str(s: &str) -> Result<Self, Self::Err> {
-        let mut s = s.trim();
+        let argv = s.split_whitespace();
+        let args = Read::parse(argv)?;
 
-        let mut expr = Self {
-            addr: NativePtr::from_ptr(0),
-            ty: Type::Array(Box::new(Type::Felt), 4),
-            count: 1,
-            mode: MemoryMode::default(),
-            format: FormatType::default(),
+        let ty = args.ty.unwrap_or_else(|| Type::Array(Box::new(Type::Felt), 4));
+        let addr = match args.mode {
+            MemoryMode::Word => NativePtr::new(args.addr, 0, 0),
+            MemoryMode::Byte => NativePtr::from_ptr(args.addr),
         };
-
-        // Parse any leading options
-        let s = parse_options(s, &mut expr)?;
-
-        let (addr, s) = parse_address(s)?;
-
-        // Parse any trailing options
-        let s = parse_options(s, &mut expr)?;
-
-        // If the remaining string is non-empty, raise an error
-        if !s.trim().is_empty() {
-            return Err(format!("invalid read expression at '{s}'"));
-        }
-
-        // Handle address conversion, etc.
-        //
-        // If the mode is Byte, convert the address to NativePtr.
-        //
-        // Otherwise we can trivially construct the NativePtr from the word address
-        expr.addr = if expr.mode == MemoryMode::Byte {
-            NativePtr::from_ptr(addr)
-        } else {
-            NativePtr::new(addr, 0, 0)
-        };
-
-        Ok(expr)
+        Ok(Self {
+            addr,
+            ty,
+            count: args.count,
+            mode: args.mode,
+            format: args.format,
+        })
     }
 }
 
-fn parse_options<'a>(s: &'a str, expr: &mut ReadMemoryExpr) -> Result<&'a str, String> {
-    let mut s = s.trim_start();
-    while let Some(rest) = s.strip_prefix('-') {
-        // Try to split FLAG from any of its possible arguments
-        let (flag, rest) = rest.split_once(' ').unwrap_or((rest, ""));
-        match flag.trim() {
-            "m" | "mode" => {
-                // Try to split MODE from rest of expression
-                match rest.split_once(' ') {
-                    Some((mode, rest)) => {
-                        expr.mode = mode.trim().parse()?;
-                        s = rest.trim_start();
-                    }
-                    None => {
-                        expr.mode = rest.trim().parse()?;
-                        s = "";
-                    }
-                }
-            }
-            "c" | "count" => {
-                // Try to split COUNT from rest of expression
-                match rest.split_once(' ') {
-                    Some((count, rest)) => {
-                        expr.count =
-                            count.trim().parse().map_err(|err| format!("invalid count: {err}"))?;
-                        s = rest.trim_start();
-                    }
-                    None => {
-                        expr.count =
-                            rest.trim().parse().map_err(|err| format!("invalid count: {err}"))?;
-                        s = "";
-                    }
-                }
-            }
-            "f" | "format" => {
-                // Try to split FORMAT from rest of expression
-                match rest.split_once(' ') {
-                    Some((format, rest)) => {
-                        expr.format = format.trim().parse()?;
-                        s = rest.trim_start();
-                    }
-                    None => {
-                        expr.format = rest.trim().parse()?;
-                        s = "";
-                    }
-                }
-            }
-            "t" | "type" => {
-                // Try to split TYPE from rest of expression
-                match rest.split_once(' ') {
-                    Some((ty, rest)) => {
-                        let (ty, rest) = parse_type(ty.trim())?;
-                        expr.ty = ty;
-                        s = rest.trim_start();
-                    }
-                    None => {
-                        let (ty, rest) = parse_type(rest.trim())?;
-                        expr.ty = ty;
-                        s = rest;
-                    }
-                }
-            }
-            _ => return Err(format!("unrecognized option: {flag}")),
-        }
-    }
+#[derive(Default, Debug, Parser)]
+#[command(name = "read")]
+pub struct Read {
+    /// The memory address to start reading from
+    #[arg(required(true), value_name = "ADDR", value_parser(parse_address))]
+    pub addr: u32,
+    /// The type of value to read from ADDR, defaults to 'word'
+    #[arg(
+        short = 't',
+        long = "type",
+        value_name = "TYPE",
+        value_parser(TypeParser)
+    )]
+    pub ty: Option<Type>,
+    /// The number of values to read
+    #[arg(short = 'c', long = "count", value_name = "N", default_value_t = 1)]
+    pub count: u8,
+    /// The addressing mode to use
+    #[arg(
+        short = 'm',
+        long = "mode",
+        value_name = "MODE",
+        default_value_t = MemoryMode::Word,
+        value_parser(MemoryModeParser)
+    )]
+    pub mode: MemoryMode,
+    /// The format to use when printing integral values
+    #[arg(
+        short = 'f',
+        long = "format",
+        value_name = "FORMAT",
+        default_value_t = FormatType::Decimal,
+        value_parser(FormatTypeParser)
+    )]
+    pub format: FormatType,
+}
+impl Read {
+    pub fn parse<I, S>(argv: I) -> Result<Self, String>
+    where
+        I: IntoIterator<Item = S>,
+        S: Into<OsString> + Clone,
+    {
+        let command = <Self as clap::CommandFactory>::command()
+            .disable_help_flag(true)
+            .disable_version_flag(true)
+            .disable_colored_help(true)
+            .no_binary_name(true);
 
-    Ok(s)
+        let mut matches = command.try_get_matches_from(argv).map_err(|err| err.to_string())?;
+        <Self as clap::FromArgMatches>::from_arg_matches_mut(&mut matches)
+            .map_err(|err| err.to_string())
+    }
 }
 
-fn parse_type(s: &str) -> Result<(Type, &str), String> {
-    Ok((
-        match s {
+#[doc(hidden)]
+#[derive(Clone)]
+struct TypeParser;
+impl clap::builder::TypedValueParser for TypeParser {
+    type Value = Type;
+
+    fn parse_ref(
+        &self,
+        _cmd: &clap::Command,
+        _arg: Option<&clap::Arg>,
+        value: &OsStr,
+    ) -> Result<Self::Value, clap::error::Error> {
+        use clap::error::{Error, ErrorKind};
+
+        let value = value.to_str().ok_or_else(|| Error::new(ErrorKind::InvalidUtf8))?;
+
+        Ok(match value {
             "i1" => Type::I1,
             "i8" => Type::I8,
             "i16" => Type::I16,
@@ -139,47 +123,39 @@ fn parse_type(s: &str) -> Result<(Type, &str), String> {
             "felt" => Type::Felt,
             "word" => Type::Array(Box::new(Type::Felt), 4),
             "ptr" | "pointer" => Type::Ptr(Box::new(Type::U32)),
-            _ => return Err(format!("invalid/unsupported type '{s}'")),
-        },
-        "",
-    ))
-}
-
-fn parse_address(s: &str) -> Result<(u32, &str), String> {
-    let mut buf = String::new();
-    let mut last_index = 0;
-    let radix = if let Some(rest) = s.strip_prefix("0x") {
-        last_index = 2;
-        let mut chars = rest.chars().peekable();
-        while let Some(c) = chars.next_if(|&c| c.is_ascii_hexdigit()) {
-            buf.push(c);
-            last_index += c.len_utf8();
-        }
-        16
-    } else {
-        let mut chars = s.chars().peekable();
-        while let Some(c) = chars.next_if(|&c| c.is_ascii_digit()) {
-            buf.push(c);
-            last_index += c.len_utf8();
-        }
-        10
-    };
-
-    if buf.is_empty() {
-        return Err(format!("expected memory address at '{s}'"));
+            _ => {
+                return Err(Error::raw(
+                    ErrorKind::InvalidValue,
+                    format!("invalid/unsupported type '{value}'"),
+                ))
+            }
+        })
     }
-
-    let addr = u32::from_str_radix(buf.as_str(), radix)
-        .map_err(|err| format!("invalid memory address: {err}"))?;
-    let (_, rest) = s.split_at_checked(last_index).unwrap_or((s, ""));
-    Ok((addr, rest.trim_start()))
 }
 
-#[derive(Default, Debug, Copy, Clone, PartialEq, Eq)]
+fn parse_address(s: &str) -> Result<u32, String> {
+    if let Some(s) = s.strip_prefix("0x") {
+        u32::from_str_radix(s, 16).map_err(|err| format!("invalid memory address: {err}"))
+    } else if s.is_empty() {
+        Err(format!("expected memory address at '{s}'"))
+    } else {
+        s.parse::<u32>().map_err(|err| format!("invalid memory address: {err}"))
+    }
+}
+
+#[derive(Default, Debug, Copy, Clone, PartialEq, Eq, ValueEnum)]
 pub enum MemoryMode {
     #[default]
     Word,
     Byte,
+}
+impl fmt::Display for MemoryMode {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        match self {
+            Self::Word => f.write_str("word"),
+            Self::Byte => f.write_str("byte"),
+        }
+    }
 }
 impl FromStr for MemoryMode {
     type Err = String;
@@ -193,12 +169,53 @@ impl FromStr for MemoryMode {
     }
 }
 
+#[doc(hidden)]
+#[derive(Clone)]
+struct MemoryModeParser;
+impl clap::builder::TypedValueParser for MemoryModeParser {
+    type Value = MemoryMode;
+
+    fn possible_values(
+        &self,
+    ) -> Option<Box<dyn Iterator<Item = clap::builder::PossibleValue> + '_>> {
+        use clap::builder::PossibleValue;
+        Some(Box::new(
+            [
+                PossibleValue::new("words").aliases(["w", "word", "miden"]),
+                PossibleValue::new("bytes").aliases(["b", "byte", "rust"]),
+            ]
+            .into_iter(),
+        ))
+    }
+
+    fn parse_ref(
+        &self,
+        _cmd: &clap::Command,
+        _arg: Option<&clap::Arg>,
+        value: &OsStr,
+    ) -> Result<Self::Value, clap::error::Error> {
+        use clap::error::{Error, ErrorKind};
+
+        let value = value.to_str().ok_or_else(|| Error::new(ErrorKind::InvalidUtf8))?;
+        value.parse().map_err(|err| Error::raw(ErrorKind::InvalidValue, err))
+    }
+}
+
 #[derive(Default, Debug, Copy, Clone, PartialEq, Eq)]
 pub enum FormatType {
     #[default]
     Decimal,
     Hex,
     Binary,
+}
+impl fmt::Display for FormatType {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        match self {
+            Self::Decimal => f.write_str("decimal"),
+            Self::Hex => f.write_str("hex"),
+            Self::Binary => f.write_str("binary"),
+        }
+    }
 }
 impl FromStr for FormatType {
     type Err = String;
@@ -210,5 +227,38 @@ impl FromStr for FormatType {
             "b" | "bin" | "binary" | "bits" => Ok(Self::Binary),
             _ => Err(format!("invalid format type '{s}'")),
         }
+    }
+}
+
+#[doc(hidden)]
+#[derive(Clone)]
+struct FormatTypeParser;
+impl clap::builder::TypedValueParser for FormatTypeParser {
+    type Value = FormatType;
+
+    fn possible_values(
+        &self,
+    ) -> Option<Box<dyn Iterator<Item = clap::builder::PossibleValue> + '_>> {
+        use clap::builder::PossibleValue;
+        Some(Box::new(
+            [
+                PossibleValue::new("decimal").alias("d"),
+                PossibleValue::new("hex").aliases(["x", "hexadecimal"]),
+                PossibleValue::new("binary").aliases(["b", "bin", "bits"]),
+            ]
+            .into_iter(),
+        ))
+    }
+
+    fn parse_ref(
+        &self,
+        _cmd: &clap::Command,
+        _arg: Option<&clap::Arg>,
+        value: &OsStr,
+    ) -> Result<Self::Value, clap::error::Error> {
+        use clap::error::{Error, ErrorKind};
+
+        let value = value.to_str().ok_or_else(|| Error::new(ErrorKind::InvalidUtf8))?;
+        value.parse().map_err(|err| Error::raw(ErrorKind::InvalidValue, err))
     }
 }
